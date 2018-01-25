78571d0072785ff9b0c0e7e3732d3fe357cf7607
Remigijus Čeikauskas <rec@trustpilot.com>
2018-01-25 16:38:37 +0200

    Fiddling with mongoose middleware

--- model/user/schema.js
+++ model/user/schema.js
@@ -1,11 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+class StoreMetaData {
+  constructor() {
+    this.created = new Date();
+  }
+}
 
 const userSchema = new Schema({
   title: { type: String, required: true },
-  body: { type: String }
+  body: { type: String },
+  metadata: {
+    created: Date
+  }
 });
 
+userSchema.pre('save', function (next) {
+  if (this.isNew) {
+    this.metadata = {
+      created: new Date()
+    };
+    console.warn('user save new', this);
+  } else {
+    console.warn('user save existing', this);
+  }
+  next();
+});
+
+userSchema.pre('update', function (next) {
+  console.warn('user update');
+  this.update({}, { $set: { metadata : new StoreMetaData() } });
+  next();
+});
 
 module.exports =  userSchema;
